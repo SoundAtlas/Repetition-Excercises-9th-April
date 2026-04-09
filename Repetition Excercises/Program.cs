5564769c76b90dab1bf2b3104641300a7cdd4a71
Andreas Hemmer <andreas.hemmer@hotmail.com>
2026-04-09 14:24:29 +0200

    Station 5 - C

--- Repetition Excercises/Program.cs
+++ Repetition Excercises/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
+            List<Employee> employees = new List<Employee>();
+            employees.Add(new FullTimeEmployee("Anna", 30000));
+            employees.Add(new PartTimeEmployee("Bo", 0, 80));
+            employees.Add(new FullTimeEmployee("Carl", 32000));
 
+            foreach (var e in employees)
+            {
+                e.ShowInfo();
+            }
         }
 
     }
